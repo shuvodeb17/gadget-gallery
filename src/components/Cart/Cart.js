d07697473d55839d0f00a2493bd1d6e7ecdf535a
shuvodeb17 <shuvodeb17014@gmail.com>
2022-11-29 22:53:59 -0800

    cart calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 const Cart = (props) => {
     const {cartOrder} = props;
+    console.log(cartOrder);
+
+    let initialValue = 0;
+    let shipping = 0;
+    let grandTotal = 0;
+    for(const product of cartOrder){
+        initialValue+=product.price;
+        shipping = shipping + product.shipping;
+        grandTotal = initialValue+shipping;
+    }
+    const tax = (initialValue * 10 / 100).toFixed(2);
+
+   
     return (
         <div>
             <h4>Order summary</h4>
             <p>Selected Items: {cartOrder.length}</p>
+            <p>Total Price: ${initialValue}</p>
+            <p>Total Shipping: {shipping} </p>
+            <p>Tax: ${tax} </p>
+            <h3>Grand Total: ${grandTotal}</h3>
         </div>
     );
 };
