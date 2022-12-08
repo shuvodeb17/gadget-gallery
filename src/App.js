0b43c410d48e749af61ee306feca3d4126483c4c
shuvodeb17 <shuvodeb17014@gmail.com>
2022-12-07 20:47:26 -0800

    react-heels-villa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
-import Product from './components/Product/Product';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Product></Product>
+      
     </div>
   );
 }
